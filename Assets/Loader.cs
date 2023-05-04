945e33000a5bcbb6f329195687b7245ebf94d506
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-04 16:39:02 +0300

    Initial commit

---
+++ Assets/Loader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Loader : MonoBehaviour
+{
+    public SpriteRenderer Renderer;
+    public string LargeTextureAddress;
+    public string SmallTextureAddress;
+    public KeyCode ContinueKey = KeyCode.Space;
+
+    private IEnumerator Start ()
+    {
+        Debug.Log("Loading large texture...");
+        var largeTextureHandle = Addressables.LoadAssetAsync<Sprite>(LargeTextureAddress);
+        while (!largeTextureHandle.IsDone) yield return null;
+        Renderer.sprite = largeTextureHandle.Result;
+        Debug.Log($"Large texture loaded. Press {ContinueKey} to continue.");
+        while (!Input.GetKeyUp(ContinueKey)) yield return null;
+
+        Debug.Log("Loading small texture...");
+        var smallTextureHandle = Addressables.LoadAssetAsync<Sprite>(SmallTextureAddress);
+        while (!smallTextureHandle.IsDone) yield return null;
+        Renderer.sprite = smallTextureHandle.Result;
+        Debug.Log($"Small texture loaded. Press {ContinueKey} to continue.");
+        while (!Input.GetKeyUp(ContinueKey)) yield return null;
+
+        Debug.Log("Releasing large texture and unloading unused assets...");
+        Addressables.Release(largeTextureHandle);
+        var unloadHandle = Resources.UnloadUnusedAssets();
+        while (!unloadHandle.isDone) yield return null;
+        Debug.Log("Large texture should now be unloaded.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AddressableUnloadIssue
+ Repro project for Unity's addressable bug when Resources.UnloadUnusedAssets not unloading released assets
